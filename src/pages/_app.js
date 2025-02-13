d836f8fd273fcc72466d882ebe182684de9327e7
Victor Lima <vtxvictoroliveira@gmail.com>
2025-02-13 09:27:42 -0300

    Atualizacao 2.2.1.1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import "@/estilos/scrollReveal.css";
+import "@/styles/scrollReveal.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
